722de15fafca7141d237220f2f25251f1e0c18f7
JessicaQueiroz <jehqueiroz01@gmail.com>
2020-11-03 21:00:34 -0300

    Initial commit

---
+++ src/com/company/Casa.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Casa {
+    // propriedades
+    String cor;
+
+    // método
+    void abrirPorta(){
+        System.out.println("Porta aberta");
+    }
+
+    void nGaragens(){
+        System.out.println(5);
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Casa minhaCasa = new Casa();
+
+	    minhaCasa.cor = "Verde";
+
+	    System.out.println(minhaCasa.cor);
+		minhaCasa.abrirPorta();
+		minhaCasa.nGaragens();
+	}
+}
